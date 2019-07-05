d145eef4a9805785e1fdce2d09608ca5fef8bf04
GautamVashishth <gautamvashishth@gmail.com>
2019-07-05 13:27:14 +0530

    git folder

---
+++ tobeaddedtogit/hello1.js
@@ -0,0 +1,19 @@
+function green() {
+    document.getElementById("demo1").innerHTML = "This is Green";
+}
+
+function blue() {
+    document.getElementById("demo2").innerHTML = "This is Blue";
+}
+
+function red() {
+    document.getElementById("demo3").innerHTML = "This is red";
+}
+
+function gray() {
+    document.getElementById("demo4").innerHTML = "This is gray";
+}
+
+function black() {
+    document.getElementById("demo5").innerHTML = "This is Black";
+}
